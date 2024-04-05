85bd1a3dc3b71b5a9d18692e9cc5663253fdaf2e
ashish140399 <ashish140399@gmail.com>
2024-04-05 23:11:21 +0530

    initial

--- src/hooks/useWalletConnection.ts
+++ src/hooks/useWalletConnection.ts
@@ -1,24 +1,34 @@
 import { useWeb3React } from "@web3-react/core";
 import { toast } from "react-toastify";
 import { NETWORK_NAME, TAG_PROVIDER } from "../libs/constants";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const useWalletConnection = () => {
   const { active, account, chainId, library, activate, deactivate } =
     useWeb3React();
 
-  const connectWallet = (wallet: any, callBack: any) => {
+  const connectWallet = async (wallet: any, callBack: any) => {
     if (!window) return;
 
     window.localStorage.clear();
     window.localStorage.setItem(TAG_PROVIDER, wallet.title);
-    if (callBack) {
-      callBack();
-    }
-    activate(wallet.connector, (error: Error) => {
-      if (error.message.includes("Unsupported chain id")) {
-        toast.info(`Please change network to ${NETWORK_NAME}.`);
+    
+    const provider = new WalletConnectProvider();
+
+    try {
+      await provider.enable();
+      activate(provider, (error: Error) => {
+        if (error && error.message.includes("Unsupported chain id")) {
+          toast.info(`Please change network to ${NETWORK_NAME}.`);
+        }
+      });
+      if (callBack) {
+        callBack();
       }
-    });
+    } catch (error) {
+      console.error("Failed to connect:", error);
+      // Handle error
+    }
   };
 
   const disconnectWallet = (callBack: any) => {
