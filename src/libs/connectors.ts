c08af22b71e80399059ba866ec865d1cab5cb5cf
ashish140399 <ashish140399@gmail.com>
2024-04-06 10:16:24 +0530

    Wallet fix

--- src/libs/connectors.ts
+++ src/libs/connectors.ts
@@ -1,12 +1,12 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
-import { Web3Wallet } from '@walletconnect/web3wallet'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { BSC_INFURA_KEY } from "./constants";
 
 export const METAMASK = new InjectedConnector({
   supportedChainIds: [56],
 });
 
-export const WALLETCONNECT = new Web3Wallet({
+export const WALLETCONNECT = new WalletConnectConnector({
   // @ts-ignore
   rpcUrl: `https://mainnet.infura.io/v3/${BSC_INFURA_KEY}`,
   bridge: "https://bridge.walletconnect.org",
